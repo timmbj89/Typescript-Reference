f99177feb6ecd1aae6306c016630bd8d1660aa72
Bryan Timm <47614226+timmbj89@users.noreply.github.com>
2020-05-11 17:32:48 +0200

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,15 @@
+//RETURNING JSON RESPONSE FROM WEB FETCH
+export async function http(
+  request: RequestInfo
+): Promise<any> {
+  const response = await fetch(request);
+  const body = await response.json();
+  return body;
+}
+
+// example consuming code
+const data = await http(
+  "https://jsonplaceholder.typicode.com/todos"
+);
+
+
